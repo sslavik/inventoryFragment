874647d8cba72db08a5fb9467032e39d4bbedd69
sslavik <sslavikshy@gmail.com>
2019-10-11 11:17:11 +0200

    Added Logging

--- app/src/main/java/com/sslavik/inventory/SplashActivity.java
+++ app/src/main/java/com/sslavik/inventory/SplashActivity.java
@@ -2,13 +2,22 @@ package com.sslavik.inventory;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
+        setContentView(R.layout.activity_splash  );
+
+        new Handler().postDelayed(new Runnable() {
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, LoginActivity.class));
+                finish();
+            }
+        }, 1000);
     }
 }
