e2d6fbff651b25a600d0b7551b1582d827c7e9df
sslavik <sslavikshy@gmail.com>
2019-10-22 12:53:27 +0200

    validate User almost corrected

--- app/src/main/java/com/sslavik/inventory/ui/SignUpActivity.java
+++ app/src/main/java/com/sslavik/inventory/ui/SignUpActivity.java
@@ -2,11 +2,16 @@ package com.sslavik.inventory.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.material.textfield.TextInputEditText;
@@ -38,10 +43,12 @@ public class SignUpActivity extends AppCompatActivity {
         tiedPassword2 = findViewById(R.id.tiedPassword2);
         tiedEmail = findViewById(R.id.tiedEmail);
         // TextInputLayout
-        tilUser = findViewById(R.id.tiedUser);
-        tilPassword1 = findViewById(R.id.tiedPassword1);
-        tilPassword2 = findViewById(R.id.tiedPassword2);
-        tilEmail = findViewById(R.id.tiedEmail);
+        tilUser = findViewById(R.id.tilUser);
+        tilPassword1 = findViewById(R.id.tilPassword1);
+        tilPassword2 = findViewById(R.id.tilPassword2);
+        tilEmail = findViewById(R.id.tilEmail);
+        // TextChanged
+        tiedUser.addTextChangedListener(new SignUpWatcher(tiedUser));
         // Evento OnClick SignUp
         btSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -55,23 +62,108 @@ public class SignUpActivity extends AppCompatActivity {
      * Valida el estado del formulario para poder insertarlo en la base de datos
      */
     private void validate() {
-        if(CommonUtils.validateUser(tiedUser.getText().toString())
-                && CommonUtils.validatePassword(tiedPassword1.getText().toString(), tiedPassword2.getText().toString())
+        if(validateUser(tiedUser)
+                && validatePassword(tiedPassword1, tiedPassword2)
                 && CommonUtils.validateEmail(tiedEmail.getText().toString())) {
             // 1- Se guarda el usuario en la BD
             // 2- Envio de correo al usuario (Firebase)
             // 3- Se pasa por la ventana LogInActivity
-            startActivity(new Intent(SignUpActivity.this, LoginActivity.class).setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP));
+            startActivity(new Intent(SignUpActivity.this, LoginActivity.class));
             // intent.set(Intent.FLAG_ACTIVITY_SINGLE_TOP) Con esto conseguiremos que la activity pueda ser lanzada solo 1 vez
 
             finish();
-        } else
-            Toast.makeText(this, "Sign Up Failed.", Toast.LENGTH_SHORT);
+            Toast.makeText(this, "SignUp Successful.", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "SignUp Failed.", Toast.LENGTH_SHORT).show();
+        }
+
     }
 
-    private void errorShow(){
-        if(CommonUtils.validateUser(tiedUser.getText().toString())) {
-            tilUser.setError(R.string.errUserEmpty);
+    /**
+     * 1. Comprueba que sean iguales
+     * 2. Que tenga minimo 8 caracteres.
+     * 3. 1 Mayuscula
+     * 4. 1 Número
+     * 5. No puede ser vacio
+     * @return Valida Contraseña
+     */
+    private boolean validatePassword(View view, View view2) {
+        String pass1 = ((TextInputEditText)view).getText().toString();
+        String pass2 = ((TextInputEditText)view2).getText().toString();
+        Log.d("SignIn", " Equals : " + pass1.equals(pass2) + "Length: " + (pass1.length() >= 8 && pass1.length() <= 12) + " mayus : " + pass1.matches(".*([A-Z]){1,}.*") + " num : " + pass1.matches(".*([0-9]){1,}.*"));
+        if(pass1.length() >= 8
+                && pass1.length() <= 12
+                && pass1.equals(pass2)
+                && pass1.matches(".*([A-Z]){1,}.*")
+                && pass1.matches(".*([0-9]){1,}.*")){
+            return true;
+        } else {
+            displaySoftKeyboard(view);
+            tilPassword1.setError(getString(R.string.errEmail));
+            return false;
+        }
+    }
+
+    /**
+     * @return Valida Nombre
+     */
+    private boolean validateUser(View view) {
+        if(((TextInputEditText)view).getText().toString().length() >= 4)
+            return true;
+        else {
+            displaySoftKeyboard(view);
+            tilUser.setError(getString(R.string.errUserEmpty));
+            return false;
+        }
+    }
+
+
+    private void displaySoftKeyboard(View view){
+        if(view.requestFocus())
+            ((InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE)).showSoftInput(view, 0);
+    }
+
+
+    private class SignUpWatcher implements TextWatcher {
+
+        private View view;
+        public SignUpWatcher(View view){
+            this.view = view;
+        }
+
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+            switch (view.getId()){
+                case R.id.tiedUser:
+                    if(!validateUser(view)){
+                        tilUser.setError(getString(R.string.errUserEmpty));
+                    }
+                    else tilUser.setError(null);
+                    break;
+                case R.id.tiedPassword1:
+                    if(!validatePassword(view, tiedPassword2)){
+                        tilPassword1.setError(getString(R.string.errPassword));
+                    }
+                    else tilPassword1.setError(null);
+                    break;
+                case R.id.tiedEmail:
+                    if(!CommonUtils.validateEmail(((TextInputEditText)view).getText().toString())){
+                        tilEmail.setError(getString(R.string.errEmail));
+                    }
+                    else tilEmail.setError(null);
+                    break;
+
+            }
         }
     }
 
